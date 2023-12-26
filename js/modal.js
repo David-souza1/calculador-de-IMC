f3fd11a8dad81df46a16335ccb8a8126ca89abe1
David Souza <108116507+David-souza1@users.noreply.github.com>
2023-12-26 17:10:18 -0300

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+//usei pra separar responsabilidades do javascript
+
+
+export const Modal = {
+
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal .title button'),
+
+    open() {
+        Modal.wrapper.classList.add('open')
+    },
+    close() {
+        Modal.wrapper.classList.remove('open')
+    }
+}
+
+Modal.buttonClose.onclick = () => {
+    Modal.close()
+}
+
+window.addEventListener("keydown", handleKeydown)
+function handleKeydown(event) {
+    if (event.key === 'Escape') {
+        Modal.close()
+    }
+}
